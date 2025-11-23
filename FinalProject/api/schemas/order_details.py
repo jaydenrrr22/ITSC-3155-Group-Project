54b356286d34ba9c79e31865457c7e322dd307e7
devin <166536125+Devinthedude515@users.noreply.github.com>
2025-11-23 17:07:10 -0500

    Updated order details

--- FinalProject/api/schemas/order_details.py
+++ FinalProject/api/schemas/order_details.py
@@ -21,7 +21,7 @@ class OrderDetailUpdate(BaseModel):
 class OrderDetail(OrderDetailBase):
     id: int
     order_id: int
-    sandwich: Sandwich = None
+    sandwich: Sandwich = Optional[Sandwich]
 
     class ConfigDict:
         from_attributes = True
