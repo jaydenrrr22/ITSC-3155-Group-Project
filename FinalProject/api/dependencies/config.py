95458d1a96e72895f389e331dbc7c0f1534078d3
brawl7787 <79094576+soberox@users.noreply.github.com>
2025-11-28 17:25:23 -0500

    resolved

--- FinalProject/api/dependencies/config.py
+++ FinalProject/api/dependencies/config.py
@@ -1,8 +1,10 @@
 class conf:
     db_host = "localhost"
     db_name = "sandwich_maker_api"
+    db_name = "order_processing"
     db_port = 3306
     db_user = "root"
     db_password = "yourpassword"
+    db_password = "gAmbfnVLxAA3$"
     app_host = "localhost"
     app_port = 8000
