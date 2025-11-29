6479b601f63052c71fb5188d99629799134074ab
devin <166536125+Devinthedude515@users.noreply.github.com>
2025-11-29 11:31:49 -0500

    Updated Orders
    Will now prompt staff once order is placed if there are not enough resources to create the order

--- FinalProject/api/dependencies/config.py
+++ FinalProject/api/dependencies/config.py
@@ -1,10 +1,8 @@
 class conf:
     db_host = "localhost"
     db_name = "sandwich_maker_api"
-    db_name = "order_processing"
     db_port = 3306
     db_user = "root"
-    db_password = "yourpassword"
-    db_password = "gAmbfnVLxAA3$"
+    db_password = "Bronzekneecap515!"
     app_host = "localhost"
     app_port = 8000
